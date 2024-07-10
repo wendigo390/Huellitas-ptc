d482c4cafc875dba8f7c9931ac313d7e86b636da
MRFLACK <jordixhernandez07@gmail.com>
2024-07-10 14:12:44 -0600

    Adopt y GivingAdoption

--- Huellitas-ptc/04_Adopt.aspx.designer.cs
+++ Huellitas-ptc/04_Adopt.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace Huellitas_ptc
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList Gender;
+
+        /// <summary>
+        /// Control ProductsLiteral.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ProductsLiteral;
     }
 }
--- Huellitas-ptc/05_GivingAdoption.aspx.cs
+++ Huellitas-ptc/05_GivingAdoption.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,6 +17,52 @@ namespace Huellitas_ptc
         protected void Gender_SelectedIndexChanged(object sender, EventArgs e)
         {
             Response.Redirect("05_GivingAdoptionEsp.aspx");
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            string strFileName;
+            string strFilePath;
+            string strFolder;
+
+            strFolder = Server.MapPath("./mascotas/");
+
+            if (foTo.HasFile)
+            {
+                strFileName = foTo.PostedFile.FileName;
+                strFileName = Path.GetFileName(strFileName);
+
+                if (!Directory.Exists(strFolder))
+                {
+                    Directory.CreateDirectory(strFolder);
+                }
+                // Save the uploaded file to the server.
+                strFilePath = strFolder + strFileName;
+                if (!File.Exists(strFilePath))
+                {
+                    foTo.PostedFile.SaveAs(strFilePath);
+                }
+                string nombre = noMbre.Text.Trim();
+                string edad = eDad.Text.Trim();
+                string peso = peSo.Text.Trim();
+                string image = strFileName;
+                string raza = raZa.Text.Trim();
+                string descripcion = comentarios.Text.Trim();
+                int guardado = conexiones.Add_pets(nombre, edad, peso, image, raza, descripcion);
+
+                if (guardado == 1)
+                {
+                    alerta.Text = "<script>swal('Éxito', 'Su mascota fue agregada', 'success').then(function() {window.location.href = '';});</script>";
+
+                }
+                else
+                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Hubo un error al guardar los datos');", true);
+
+            }
+        }
+        protected void foTo_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
